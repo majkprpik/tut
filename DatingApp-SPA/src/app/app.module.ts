7ea13695cd0ae6ee6ba672c87102d69f3aa5309a
majkprpik <vedran.prpic1@gmail.com>
2019-03-01 13:38:41 +0100

    sorting and filtering

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BsDropdownModule, TabsModule, BsDatepickerModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule, BsDatepickerModule, PaginationModule, ButtonsModule } from 'ngx-bootstrap';
 import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule } from 'ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
@@ -61,6 +61,8 @@ export function tokenGetter() {
       RouterModule.forRoot(appRoutes),
       BsDatepickerModule.forRoot(),
       NgxGalleryModule,
+      PaginationModule.forRoot(),
+      ButtonsModule.forRoot(),
       FileUploadModule,
       JwtModule.forRoot({
          config: {
